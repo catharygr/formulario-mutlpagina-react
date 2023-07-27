aa5a3d7c5c763a9478c452402de8eb5f1c1ae4f2
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-27 13:58:11 +0100

    Trabajando en el componente Header y css

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,17 +1,21 @@
 import { Menu } from "react-feather";
 import { Umbrella } from "react-feather";
 
-import NavBar from "./NavBar";
+// import NavBar from "./NavBar";
 
 export default function Header() {
   return (
-    <header className="header-container">
-      <div className="header-logo">
-        <Umbrella size={30} />
-        <h1>Weather App</h1>
+    <header>
+      <div className="header-container">
+        <div className="header-logo">
+          <Umbrella size={30} color="var(--color-green)" strokeWidth={3} />
+          <h1>Weather App</h1>
+        </div>
+        <div className="header-menu">
+          <Menu size={30} />
+          {/* <NavBar /> */}
+        </div>
       </div>
-      <Menu size={30} />
-      {/* <NavBar /> */}
     </header>
   );
 }
