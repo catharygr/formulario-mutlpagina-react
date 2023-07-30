71695eb69b39fe0b22ae25a5243e7dea26dfa961
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-30 18:10:15 +0100

    Trabajando con el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./componentes/Header";
 import OfertasTrabajo from "./componentes/OfertasTrabajo";
+import Footer from "./componentes/Footer";
 
 function App() {
   return (
     <div className="app-container">
       <Header />
       <OfertasTrabajo />
+      <Footer />
     </div>
   );
 }
