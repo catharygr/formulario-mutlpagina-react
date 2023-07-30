5d9d207a8704a3be997724fd5e1b10bcb8386321
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-30 17:32:43 +0100

    Trabajando con css

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,17 +1,20 @@
 export const data = [
   {
+    id: "1",
     title: "Mago del Clima",
     description:
       "¡Atención a todos los fanáticos del clima! Necesitamos un Mago del Clima para trabajar su magia con los datos y predecir lo impredecible. Si puedes bailar con los datos como si nadie estuviera mirando y hacer pronósticos del clima en los que incluso las ardillas confían, ¡te queremos a ti! Salario competitivo para financiar tu adicción al café.",
     salary: "80,000€ a 120,000€",
   },
   {
+    id: "2",
     title: "Ninja del Código Meteorológico",
     description:
       "Se busca: Genio de la codificación con un toque de meteorología. Únete a nuestro equipo de guerreros del clima para crear software que transforme datos en pronósticos deslumbrantes. Si puedes codificar algoritmos mientras malabarees con gotas de lluvia, y buscas dominar el mundo del clima con tus habilidades tecnológicas, ¡postúlate ahora! Atractivo paquete salarial y gadgets meteorológicos secretos incluidos.",
     salary: "90,000€ a 130,000€",
   },
   {
+    id: "3",
     title: "Einstein del Clima",
     description:
       "¿Eres un genio del clima que habla el lenguaje de las nubes y el viento? Necesitamos un Einstein del Clima para desentrañar los misterios atmosféricos y mejorar nuestro juego de pronósticos. Si sueñas con conquistar los cielos con tu brillantez científica y deseas un salario que esté a la altura de tu capacidad intelectual, ¡hablemos!",
