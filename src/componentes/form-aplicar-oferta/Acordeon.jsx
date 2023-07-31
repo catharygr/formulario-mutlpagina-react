1f255cb2d66341dc773f2039bb02f97c389a1b1e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-31 18:43:54 +0100

    Trabajando con el componente Acordeon y css

--- src/componentes/form-aplicar-oferta/Acordeon.jsx
+++ src/componentes/form-aplicar-oferta/Acordeon.jsx
@@ -5,12 +5,12 @@ import { ChevronDown } from "react-feather";
 export default function Acordeon({ encabezado, contenido, id }) {
   return (
     <Accordion.Root type="single" collapsible={true}>
-      <Accordion.Item>
-        <Accordion.Header>
-          <Accordion.Trigger key={id} value={id}>
-            <ChevronDown /> {encabezado}
-          </Accordion.Trigger>
-        </Accordion.Header>
+      <Accordion.Item value="item-1">
+        {/* <Accordion.Header> */}
+        <Accordion.Trigger key={id} value={id}>
+          <ChevronDown /> {encabezado}
+        </Accordion.Trigger>
+        {/* </Accordion.Header> */}
         <Accordion.Content>{contenido}</Accordion.Content>
       </Accordion.Item>
     </Accordion.Root>
