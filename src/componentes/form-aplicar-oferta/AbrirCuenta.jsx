b5c311e5ed244fc45740e60706e8b36287a0c6c2
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-01 12:45:06 +0100

    Acordeón terminado

--- src/componentes/form-aplicar-oferta/AbrirCuenta.jsx
+++ src/componentes/form-aplicar-oferta/AbrirCuenta.jsx
@@ -1,9 +1,9 @@
 import Acordeon from "./Acordeon";
 
 const dataAcordeon = {
-  encabezado: "Explicación de la oferta",
+  encabezado: "¿Por qué crear otra cuenta?",
   contenido:
-    "No vamos a recopilar ninguno de tus datos personales antes de que crees tu cuenta. ¿Puedes preguntar por qué? Bueno, eso te permitirá poder eliminar todos tus datos personales después de que el proceso de selección haya terminado, ya sea exitoso para ambas partes o no. No compartimos esos datos con nadie más que nuestro reclutador. Él es el único autorizado para revisarlos. Así que, regístrate y veamos si podemos trabajar juntos y al final hay un gran botón rojo en tu cuenta que eliminará tus datos si así lo deseas.",
+    "No vamos a recopilar ninguno de tus datos personales antes de que crees tu cuenta. \n\n ¿Puedes preguntar por qué? Bueno, eso te permitirá poder eliminar todos tus datos personales después de que el proceso de selección haya terminado, ya sea exitoso para ambas partes o no. No compartimos esos datos con nadie más que nuestro reclutador. \n\n Él es el único autorizado para revisarlos. Así que, regístrate y veamos si podemos trabajar juntos y al final hay un gran botón rojo en tu cuenta que eliminará tus datos si así lo deseas.",
 };
 
 export default function AbrirCuenta() {
--- src/componentes/form-aplicar-oferta/Acordeon.jsx
+++ src/componentes/form-aplicar-oferta/Acordeon.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable react/prop-types */
 import * as Accordion from "@radix-ui/react-accordion";
+import ReactMarkdown from "react-markdown";
 import { ChevronDown } from "react-feather";
 
-export default function Acordeon({ encabezado, contenido, id }) {
+export default function Acordeon({ encabezado, contenido }) {
   return (
     <Accordion.Root type="single" collapsible={true}>
-      <Accordion.Item value="item-1">
-        {/* <Accordion.Header> */}
-        <Accordion.Trigger key={id} value={id}>
-          <ChevronDown /> {encabezado}
-        </Accordion.Trigger>
-        {/* </Accordion.Header> */}
-        <Accordion.Content>{contenido}</Accordion.Content>
+      <Accordion.Item className="acordeon-border" value="item-1">
+        <Accordion.Header asChild>
+          <h4 className="acordeon-h4">
+            <Accordion.Trigger className="acordeon-trigger">
+              <ChevronDown /> {encabezado}
+            </Accordion.Trigger>
+          </h4>
+        </Accordion.Header>
+        <Accordion.Content className="acordeon-contenido">
+          <ReactMarkdown>{contenido}</ReactMarkdown>
+        </Accordion.Content>
       </Accordion.Item>
     </Accordion.Root>
   );
